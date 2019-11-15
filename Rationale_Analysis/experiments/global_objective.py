4ab97004bc765c5fa39d150f033bbbc28d126591
Yuval Pinter <uvp@gatech.edu>
2019-11-15 17:36:34 -0500

    max-based global objectives

---
+++ Rationale_Analysis/experiments/global_objective.py
@@ -0,0 +1,48 @@
+'''
+Util functions providing logics for extracting global rationales from a dataset.
+The input to all functions is a list of lists of importance scores (as numpy arrays), the output is arguments for extraction from each.
+These can be input in a module implementing RationaleExtractor, for extract_rationale().
+'''
+import math
+import numpy as np
+
+
+def global_argsort(arr):
+    '''
+    :returns: tupled array of ordered indices
+    '''
+    n,l = arr.shape
+    return [(i//l,i%l) for i in arr.reshape(1,-1).argsort()][0]
+
+
+def max_unconstrained(weights, lengths, max_ratio):
+    max_tokens = math.ceil(sum(lengths) * max_ratio)
+    glob_sort = global_argsort(weights)
+    return tuple([g[-max_tokens:] for g in glob_sort])  # indexes into sentence tokens
+
+
+def max_limited_min(weights, lengths, max_ratio, min_inst_ratio):
+    '''
+    first fill up on min_inst_ratio from each instance, then add the rest
+    :param min_inst_ratio: threshold of minimal words to extract from each instance
+    '''
+    n = weights.shape[0]
+    glob_sort = global_argsort(weights)
+    rev_glob_sort = reversed([(glob_sort[0][i], glob_sort[1][i]) for i in range(len(glob_sort[0]))])
+    remaining = [math.ceil(l * min_inst_ratio) for l in lengths]
+    max_tokens = math.ceil(sum(length) * max_ratio)
+    buff = []  # buffer for at-threshold instance tokens
+    tok_idcs = []  # return indices
+    while max(remaining) > 0 and len(tok_idcs) < max_tokens:
+        for tup in rev_glob_sort:
+            n, t = tup
+            if remaining[n] <= 0:
+                buff.append(tup)
+            else:
+                tok_idcs.append(tup)
+                remaining[n] -= 1
+    tok_idcs.extend(buff)
+
+    return tok_idcs[:max_tokens]
+
+
