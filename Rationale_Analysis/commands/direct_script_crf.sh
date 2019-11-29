df4ec6933ae413f96f7894e8c8d394e90e4ff9d1
Sarthak Jain <successar@gmail.com>
2019-11-28 23:28:00 -0500

    Direct

--- Rationale_Analysis/commands/direct_script_crf.sh
+++ Rationale_Analysis/commands/direct_script_crf.sh
@@ -5,7 +5,7 @@ for saliency in wrapper simple_gradient;
         do
         SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct \
         RATIONALE_CLASSIFIER=bert_generator_saliency RC_EXP_NAME=direct \
-        bash Rationale_Analysis/commands/rationale_extractor_script.sh;
+        bash Rationale_Analysis/commands/rationale_train_and_predict_script.sh;
 
         SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct \
         RATIONALE_CLASSIFIER=bert_generator_saliency RC_EXP_NAME=direct \
