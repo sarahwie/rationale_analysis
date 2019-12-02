2881dc1f1a9c417ff43774cdeba3467a2ef80790
Sarthak Jain <successar@gmail.com>
2019-12-01 23:43:16 -0500

    Timing

--- Rationale_Analysis/commands/direct_script_global.sh
+++ Rationale_Analysis/commands/direct_script_global.sh
@@ -5,10 +5,10 @@ for saliency in wrapper simple_gradient;
         do
         MIN_INST_PERCENT=10 CUDA_DEVICE=-1 BSIZE=100000 SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct bash Rationale_Analysis/commands/rationale_extractor_script.sh;
 
-        SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct sbatch Cluster_scripts/multi_gpu_sbatch.sh bash Rationale_Analysis/commands/model_b_train_script.sh;
+        SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct sbatch --time=$( t2sd ) Cluster_scripts/multi_gpu_sbatch.sh bash Rationale_Analysis/commands/model_b_train_script.sh;
         
         SALIENCY=$saliency RATIONALE=$rationale RATIONALE_EXP_NAME=direct \
         RATIONALE_CLASSIFIER=bert_generator_saliency RC_EXP_NAME=direct USE_CRF=true \
-        sbatch Cluster_scripts/multi_gpu_sbatch.sh bash Rationale_Analysis/commands/rationale_train_and_predict_script.sh;
+        sbatch --time=$( t2sd ) Cluster_scripts/multi_gpu_sbatch.sh bash Rationale_Analysis/commands/rationale_train_and_predict_script.sh;
         done;
     done;
