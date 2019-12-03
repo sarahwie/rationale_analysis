695b75c74256a1e4cb2c0b8cacd1374e97dcf5de
Sarthak Jain <successar@gmail.com>
2019-12-03 11:32:05 -0500

    Changes to Result Code

--- Rationale_Analysis/experiments/direct_results.py
+++ Rationale_Analysis/experiments/direct_results.py
@@ -161,7 +161,7 @@ def analyse_globality(values):
     values["global"] = values["rationale"].apply(lambda x: "global" in x)
     values["rationale"] = values["rationale"].apply(lambda x: m[x])
 
-    values = values.groupby(["dataset", "saliency", "rationale"]).agg(
+    values = values.groupby(["dataset", "saliency", "rationale"]).apply(
         lambda x: ttest_ind(x[x["global"] == "True"]['value'], x[x["global"] == "False"]['value'], equal_var=False)
     )
     print(values)
