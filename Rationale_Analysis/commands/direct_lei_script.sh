1fc51c83fd582ffdff4dcfe4085c69ccf1f5190b
Sarthak Jain <successar@gmail.com>
2019-12-04 10:39:28 -0500

    Second Cut

--- Rationale_Analysis/commands/direct_lei_script.sh
+++ Rationale_Analysis/commands/direct_lei_script.sh
@@ -1,4 +1,7 @@
 for rationale in top_k max_length;
     do
-    RATIONALE=$rationale RATIONALE_EXP_NAME=direct bash Rationale_Analysis/commands/model_a_rationale_extractor.sh;
+    CLASSIFIER=bert_encoder_generator \
+    RATIONALE=$rationale \
+    RATIONALE_EXP_NAME=direct \
+    bash Rationale_Analysis/commands/model_a_rationale_extractor.sh;
     done;
