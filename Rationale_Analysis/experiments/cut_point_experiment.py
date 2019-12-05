eab6af8ec86fb6562c1060c60576e95a7334a6d5
Sarthak Jain <successar@gmail.com>
2019-12-04 19:04:47 -0500

    Learning curve

--- Rationale_Analysis/experiments/cut_point_experiment.py
+++ Rationale_Analysis/experiments/cut_point_experiment.py
@@ -174,8 +174,9 @@ def results(args):
 
     for c, _ in enumerate(datasets.items()) :
         thresh = cut_point_thresh[c]
-        ax[0, c].set_xticklabels(thresh)
-
+        ax.axes[0, c].set_xticklabels(thresh)
+        ax.axes[0, c].set_xlabel("")
+        
     plt.ylim(args.min_scale, args.max_scale)
     plt.tight_layout()
     plt.legend().remove()
