d8d62139d22fded7be94a9050055eb061eb71e38
Sarthak Jain <successar@gmail.com>
2019-12-09 00:09:28 -0500

    Correction

--- Rationale_Analysis/commands/rationale_predict_human_script.sh
+++ Rationale_Analysis/commands/rationale_predict_human_script.sh
@@ -1,9 +1,9 @@
 export CONFIG_FILE=Rationale_Analysis/training_config/rationale_generators/bert_generator_human.jsonnet
 export CUDA_DEVICE=${CUDA_DEVICE:?"set cuda device"}
 
-export TRAIN_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/train.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/train.jsonl"
-export DEV_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/dev.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/dev.jsonl"
-export TEST_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/test.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/test.jsonl"
+export TRAIN_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/train.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/train.jsonl"
+export DEV_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/dev.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/dev.jsonl"
+export TEST_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/test.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/test.jsonl"
 
 export OUTPUT_BASE_PATH=${OUTPUT_BASE_PATH:-outputs}/bert_generator_human/no_crf_${HUMAN_PROB}
 
--- Rationale_Analysis/commands/rationale_train_human_script.sh
+++ Rationale_Analysis/commands/rationale_train_human_script.sh
@@ -2,9 +2,9 @@ export CONFIG_FILE=Rationale_Analysis/training_config/rationale_generators/bert_
 export CUDA_DEVICE=${CUDA_DEVICE:?"set cuda device"}
 export USE_CRF=false
 
-export TRAIN_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/train.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/train.jsonl"
-export DEV_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/dev.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/dev.jsonl"
-export TEST_DATA_PATH="${DATASET_FOLDER}/${DATASET_NAME}/data/test.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/test.jsonl"
+export TRAIN_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/train.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/train.jsonl"
+export DEV_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/dev.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/dev.jsonl"
+export TEST_DATA_PATH="${DATASETS_FOLDER}/${DATASET_NAME}/data/test.jsonl;${OUTPUT_BASE_PATH:?"set data base path"}/test.jsonl"
 
 export OUTPUT_BASE_PATH=${OUTPUT_BASE_PATH:-outputs}/bert_generator_human/no_crf_${HUMAN_PROB}
 
